cfbd4c15a2064e8953b51a89bc18a253e32e4760
MatTaylor <9727559-MatTaylor@users.noreply.replit.com>
2023-09-30 13:06:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+# from replit import db, clear
+import json
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "suckyourmumontuesdays"
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+  return render_template("login.html")
+
+
+socketio.run(app, host='0.0.0.0', port=81, debug=True, use_reloader=False)
