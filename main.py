933fb680b0fd07632937f76151b023fcfe117924
MathewPTaylor <142932068+MathewPTaylor@users.noreply.github.com>
2023-10-02 12:36:21 +0000

    committing files to new branch

--- main.py
+++ main.py
@@ -17,4 +17,9 @@ def index():
 def login():
   return render_template("login.html")
 
+
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
+  
 socketio.run(app, host='0.0.0.0', port=81, debug=True, use_reloader=False)
